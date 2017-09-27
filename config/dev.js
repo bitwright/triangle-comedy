0b79819dbae8fd02ae8abcdb559ab13421ad75f8
Hunter Wright <hunterbwright@gmail.com>
2017-09-27 14:05:08 -0400

    remove development keys file from git

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientId: '440739620334-tn53hv5d9u55ep3q94jake18929kfud4.apps.googleusercontent.com',
-  googleClientSecret: '9HNNeTdTkELIOlxKTES5lPox',
-  mongoUri: 'mongodb://hunter:thanksforallthefish@ds127044.mlab.com:27044/triangle-comedy-dev',
-  cookieKey: '12j4klmnaAjeronaNZX',
-  redirectDomain: 'http://localhost:3000'
-};
